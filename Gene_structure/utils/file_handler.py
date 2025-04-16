9b69d7a7a0c523d75bf4c408d4a185b49ca8c6fe
Prerak Khandelwal <109911183+PrerakKhandelwal@users.noreply.github.com>
2025-04-17 00:02:23 +0530

    Delete Gene_structure directory

--- Gene_structure/utils/file_handler.py
+++
@@ -1,9 +0,0 @@
-def load_dna_file(file):
-    content = file.read().decode("utf-8")
-    lines = content.strip().split("\n")
-    if lines[0].startswith(">"):
-        lines = lines[1:]
-    dna = "".join(lines).replace(" ", "").upper()
-    return "".join(filter(lambda x: x in "ATCG", dna))
-
-
--- Gene_structure/utils/transcriber.py
+++
@@ -1,3 +0,0 @@
-def transcribe_dna_to_rna(dna_sequence):
-    """Convert DNA sequence to RNA sequence."""
-    return dna_sequence.upper().replace('T', 'U')
